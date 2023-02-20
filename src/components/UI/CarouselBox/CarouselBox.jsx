bb2a46934f38a63bbdfd4bc41bdb3bb18adf0c79
Yuri Trondin <manctzns@gmail.com>
2023-02-20 10:06:24 +0500

    добавил красивый слайдер

--- src/components/UI/CarouselBox/CarouselBox.jsx
+++ src/components/UI/CarouselBox/CarouselBox.jsx
@@ -4,81 +4,64 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
-const data = [
-  {
-    id: 1,
-    title: 'узи для лазертаг',
-    image: '/images/slider/1.jpg',
-  },
-  {
-    id: 2,
-    title: 'КОРПУС ПРИБОРА',
-    image: '/images/slider/2.jpg',
-  },
-  {
-    id: 3,
-    title: 'КОРПУС ПРИБОРА',
-    image: '/images/slider/3.jpg',
-  },
-  {
-    id: 4,
-    title: 'МАКЕТ МЕТЕОРОЛОГИЧЕСКОГО ОБОРУДОВАНИЯ',
-    image: '/images/slider/4.jpg',
-  },
-  {
-    id: 5,
-    title: 'USB-НАКЛАДКА',
-    image: '/images/slider/5.jpg',
-  },
-];
+
 const CarouselBox = () => {
   const settings = {
+    arrows: false,
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    initialSlide: 0,
-    responsive: [
-      // {
-      //   breakpoint: 1024,
-      //   settings: {
-      //     slidesToShow: 2,
-      //     slidesToScroll: 2,
-      //     infinite: true,
-      //     dots: true,
-      //   },
-      // },
-      // {
-      //   breakpoint: 600,
-      //   settings: {
-      //     slidesToShow: 2,
-      //     slidesToScroll: 2,
-      //     initialSlide: 2,
-      //   },
-      // },
-      // {
-      //   breakpoint: 480,
-      //   settings: {
-      //     slidesToShow: 1,
-      //     slidesToScroll: 1,
-      //   },
-      // },
-    ],
+    autoplay: true,
+    autoplaySpeed: 5000,
   };
-  const csl = [styles.card];
 
   return (
-    <div className={styles.slider_container}>
+    <div className={styles.container}>
       <Slider {...settings}>
-        {data.map((i) => {
-          return (
-            <div key={i.id} className={styles.card}>
-              <Image src={i.image} width={280} height={158} alt="pikcha" />
-              <a>{i.title}</a>
-            </div>
-          );
-        })}
+        <div className={styles.item}>
+          <div className={styles.info}>
+            <h3>Узи для лазертага</h3>
+            <button>Подробнее</button>
+          </div>
+          <Image src="/images/slider/1.jpg" width={341} height={192} alt="pic" />
+        </div>
+        <div className={styles.item}>
+          <div className={styles.info}>
+            <h3>Узи для лазертага</h3>
+            <button>Подробнее</button>
+          </div>
+          <Image src="/images/slider/1.jpg" width={341} height={192} alt="pic" />
+        </div>
+        <div className={styles.item}>
+          <div className={styles.info}>
+            <h3>Узи для лазертага</h3>
+            <button>Подробнее</button>
+          </div>
+          <Image src="/images/slider/1.jpg" width={341} height={192} alt="pic" />
+        </div>
+        <div className={styles.item}>
+          <div className={styles.info}>
+            <h3>Узи для лазертага</h3>
+            <button>Подробнее</button>
+          </div>
+          <Image src="/images/slider/1.jpg" width={341} height={192} alt="pic" />
+        </div>
+        <div className={styles.item}>
+          <div className={styles.info}>
+            <h3>Узи для лазертага</h3>
+            <button>Подробнее</button>
+          </div>
+          <Image src="/images/slider/1.jpg" width={341} height={192} alt="pic" />
+        </div>
+        <div className={styles.item}>
+          <div className={styles.info}>
+            <h3>Узи для лазертага</h3>
+            <button>Подробнее</button>
+          </div>
+          <Image src="/images/slider/1.jpg" width={341} height={192} alt="pic" />
+        </div>
       </Slider>
     </div>
   );
