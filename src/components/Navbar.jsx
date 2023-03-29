20057af2597650097f26375afc036c6419f69df4
Yuri Trondin <manctzns@gmail.com>
2023-03-29 15:26:13 +0500

    adaptive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,6 @@ import styles from '../styles/Navbar.module.scss';
 
 const navigation = [
   { id: 1, title: 'Главная', path: '/' },
-  // { id: 2, title: 'Вакуумное литье', path: '/vacuum' },
-  // { id: 3, title: '3D-прототипирование', path: '/3dPrototype' },
   { id: 4, title: 'Выполненные работы', path: '/done' },
   { id: 5, title: 'Условия заказа', path: '/conditions' },
   { id: 6, title: 'Контакты', path: '/contacts' },
